e2555b5ec0cc66b4d20c42eeb4510f7653ae0bc5
arsjobus <arsjobus@outlook.com>
2025-11-09 17:14:22 +0700

    feat: add pulsate effect to make sure mipmap is actually working

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
 #include <iostream>
+#include <cmath> // for sinf
 
 int main() {
     sf::Vector2f windowSize = {1280.f, 720.f};
@@ -15,14 +16,13 @@ int main() {
               << window.getSettings().majorVersion << "." 
               << window.getSettings().minorVersion << std::endl;
 
-    // Check that shaders are available
     if (!sf::Shader::isAvailable()) {
          std::cerr << "Shaders are not available on this system!" << std::endl;
          return -1;
     }
 
-    // Set the frame rate limit
     window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
 
     sf::Texture texture;
     if (!texture.loadFromFile("resources/grasstexture_0.png"))
@@ -43,15 +43,17 @@ int main() {
         return -1;
     }
 
-    sf::RectangleShape rectangle(sf::Vector2f(256.f, 256.f));
+    sf::RectangleShape rectangle(sf::Vector2f(512.f, 512.f));
     rectangle.setTexture(&texture);
-    rectangle.setPosition({
-        (windowSize.x - rectangle.getSize().x) / 2.f,
-        (windowSize.y - rectangle.getSize().y) / 2.f
-    });
+
+    // Set origin to center so scaling happens around the center
+    rectangle.setOrigin(rectangle.getSize() / 2.f);
+    rectangle.setPosition({windowSize.x / 2.f, windowSize.y / 2.f});
 
     shader.setUniform("image", texture);
     shader.setUniform("iResolution", sf::Glsl::Vec2(windowSize.x, windowSize.y));
+
+    sf::Clock clock; // track elapsed time for animation
 
     // Main loop
     while (window.isOpen())
@@ -59,9 +61,7 @@ int main() {
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
             else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
             {
                 if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
@@ -69,11 +69,13 @@ int main() {
             }
         }
 
-        // Remainder of main loop
+        // Compute pulsating scale
+        float time = clock.getElapsedTime().asSeconds();
+        float scale = 1.f + 0.25f * std::sinf(time * 2.f * 3.14159f); // pulsate between 0.75x and 1.25x
+        rectangle.setScale({scale, scale});
+
         window.clear(sf::Color::Black);
-
         window.draw(rectangle, &shader);
-
         window.display();
     }
 
